b74d4649e97a2d0dfbde38d1a9c8538100419f10
Aren Sandersen <aren@foxpass.com>
2023-12-29 10:20:48 -0800

    Remove utf8_fail_as_bytes as an option; always do so
    # Conflicts:
    #	src/modules/rlm_python3/rlm_python3.c

--- src/modules/rlm_python3/rlm_python3.c
+++ src/modules/rlm_python3/rlm_python3.c
@@ -94,7 +94,6 @@ static CONF_PARSER module_config[] = {
         { "cext_compat", FR_CONF_OFFSET(PW_TYPE_BOOLEAN, rlm_python_t, cext_compat), "yes" },
         { "pass_all_vps", FR_CONF_OFFSET(PW_TYPE_BOOLEAN, rlm_python_t, pass_all_vps), "no" },
         { "pass_all_vps_dict", FR_CONF_OFFSET(PW_TYPE_BOOLEAN, rlm_python_t, pass_all_vps_dict), "no" },
-        { "utf8_fail_as_bytes", FR_CONF_OFFSET(PW_TYPE_BOOLEAN, rlm_python_t, utf8_fail_as_bytes), "no" },
 
 	CONF_PARSER_TERMINATOR
 };
@@ -385,7 +384,7 @@ static void mod_vptuple(TALLOC_CTX *ctx, REQUEST *request, VALUE_PAIR **vps, PyO
  *	Pass the value-pair print strings in a tuple.
  *
  */
-static int mod_populate_vptuple(PyObject *pPair, VALUE_PAIR *vp, bool utf8_fail_as_bytes)
+static int mod_populate_vptuple(PyObject *pPair, VALUE_PAIR *vp)
 {
 	PyObject *pStr = NULL;
 	char buf[1024];
@@ -418,20 +417,15 @@ static int mod_populate_vptuple(PyObject *pPair, VALUE_PAIR *vp, bool utf8_fail_
 		if (PyErr_Occurred()) {
 			if (PyErr_ExceptionMatches(PyExc_UnicodeDecodeError)) {
 				PyErr_Clear();
-				if (utf8_fail_as_bytes) {
-					DEBUG("Conversion to Unicode failed, returning %s as bytes", vp->da->name);
-					pStr = PyBytes_FromString(buf);
-					if (pStr == NULL) {
-						ERROR("%s:%d, vp->da->name: %s", __func__, __LINE__, vp->da->name);
-						/* Did we get another exception while handling the UnicodeDecodeError? */
-						if (PyErr_Occurred()) {
-							python_error_log();
-							PyErr_Clear();
-						}
-						return -1;
+				DEBUG("Conversion to Unicode failed, returning %s as bytes", vp->da->name);
+				pStr = PyBytes_FromString(buf);
+				if (pStr == NULL) {
+					ERROR("%s:%d, vp->da->name: %s", __func__, __LINE__, vp->da->name);
+					/* Did we get another exception while handling the UnicodeDecodeError? */
+					if (PyErr_Occurred()) {
+						python_error_log();
+						PyErr_Clear();
 					}
-				} else {
-					ERROR("String is invalid utf8, use 'utf8_fail_as_bytes' config to return as bytes");
 					return -1;
 				}
 			/* Exception was not UnicodeDecodeError */
@@ -455,7 +449,7 @@ static int mod_populate_vptuple(PyObject *pPair, VALUE_PAIR *vp, bool utf8_fail_
  * the indicated position in the tuple pArgs.
  * Returns false on error.
  */
-static bool mod_populate_vps(PyObject* pArgs, const int pos, VALUE_PAIR *vps, bool utf8_fail_as_bytes)
+static bool mod_populate_vps(PyObject* pArgs, const int pos, VALUE_PAIR *vps)
 {
 	PyObject *vps_tuple = NULL;
 	int tuplelen = 0;
@@ -494,7 +488,7 @@ static bool mod_populate_vps(PyObject* pArgs, const int pos, VALUE_PAIR *vps, bo
 			goto error;
 		}
 
-		if (mod_populate_vptuple(pPair, vp, utf8_fail_as_bytes) == 0) {
+		if (mod_populate_vptuple(pPair, vp) == 0) {
 			/* Put the tuple inside the container */
 			PyTuple_SET_ITEM(vps_tuple, i, pPair);
 		} else {
@@ -511,7 +505,7 @@ error:
 	return false;
 }
 
-static rlm_rcode_t do_python_single(REQUEST *request, PyObject *pFunc, char const *funcname, bool pass_all_vps, bool pass_all_vps_dict, bool utf8_fail_as_bytes)
+static rlm_rcode_t do_python_single(REQUEST *request, PyObject *pFunc, char const *funcname, bool pass_all_vps, bool pass_all_vps_dict)
 {
 	PyObject	*pRet = NULL;
 	PyObject	*pArgs = NULL;
@@ -534,10 +528,10 @@ static rlm_rcode_t do_python_single(REQUEST *request, PyObject *pFunc, char cons
 
 	/* If there is a request, fill in the first 4 attribute lists */
 	if (request != NULL) {
-		if (!mod_populate_vps(pArgs, 0, request->packet->vps, utf8_fail_as_bytes) ||
-		    !mod_populate_vps(pArgs, 1, request->reply->vps, utf8_fail_as_bytes) ||
-		    !mod_populate_vps(pArgs, 2, request->config, utf8_fail_as_bytes) ||
-		    !mod_populate_vps(pArgs, 3, request->state, utf8_fail_as_bytes)) {
+		if (!mod_populate_vps(pArgs, 0, request->packet->vps) ||
+		    !mod_populate_vps(pArgs, 1, request->reply->vps) ||
+		    !mod_populate_vps(pArgs, 2, request->config) ||
+		    !mod_populate_vps(pArgs, 3, request->state)) {
 
 			ERROR("%s:%d, %s - mod_populate_vps failed", __func__, __LINE__, funcname);
 			ret = RLM_MODULE_FAIL;
@@ -547,7 +541,7 @@ static rlm_rcode_t do_python_single(REQUEST *request, PyObject *pFunc, char cons
 #ifdef WITH_PROXY
 		/* fill proxy vps */
 		if (request->proxy) {
-			if (!mod_populate_vps(pArgs, 4, request->proxy->vps, utf8_fail_as_bytes)) {
+			if (!mod_populate_vps(pArgs, 4, request->proxy->vps)) {
 				ERROR("%s:%d, %s - mod_populate_vps failed", __func__, __LINE__, funcname);
 				ret = RLM_MODULE_FAIL;
 				goto finish;
@@ -555,13 +549,13 @@ static rlm_rcode_t do_python_single(REQUEST *request, PyObject *pFunc, char cons
 		} else
 #endif
 		{
-			mod_populate_vps(pArgs, 4, NULL, utf8_fail_as_bytes);
+			mod_populate_vps(pArgs, 4, NULL);
 		}
 
 #ifdef WITH_PROXY
 		/* fill proxy_reply vps */
 		if (request->proxy_reply) {
-			if (!mod_populate_vps(pArgs, 5, request->proxy_reply->vps, utf8_fail_as_bytes)) {
+			if (!mod_populate_vps(pArgs, 5, request->proxy_reply->vps)) {
 				ERROR("%s:%d, %s - mod_populate_vps failed", __func__, __LINE__, funcname);
 				ret = RLM_MODULE_FAIL;
 				goto finish;
@@ -569,12 +563,12 @@ static rlm_rcode_t do_python_single(REQUEST *request, PyObject *pFunc, char cons
 		} else
 #endif
 		{
-			mod_populate_vps(pArgs, 5, NULL, utf8_fail_as_bytes);
+			mod_populate_vps(pArgs, 5, NULL);
 		}
 
 	}
 	/* If there is no request, set all the elements to None */
-	else for (i = 0; i < 6; i++) mod_populate_vps(pArgs, i, NULL, utf8_fail_as_bytes);
+	else for (i = 0; i < 6; i++) mod_populate_vps(pArgs, i, NULL);
 
 	/*
 	 * Call Python function. If pass_all_vps_dict is true, a dictionary with the
@@ -856,7 +850,7 @@ static rlm_rcode_t do_python(rlm_python_t *inst, REQUEST *request, PyObject *pFu
 	RDEBUG3("Using thread state %p", this_thread->state);
 
 	PyEval_RestoreThread(this_thread->state);	/* Swap in our local thread state */
-	ret = do_python_single(request, pFunc, funcname, inst->pass_all_vps, inst->pass_all_vps_dict, inst->utf8_fail_as_bytes);
+	ret = do_python_single(request, pFunc, funcname, inst->pass_all_vps, inst->pass_all_vps_dict);
 	PyEval_SaveThread();
 
 	return ret;
@@ -1377,7 +1371,7 @@ static int mod_instantiate(CONF_SECTION *conf, void *instance)
 	 */
 	if (!check_config && inst->instantiate.module_name && inst->instantiate.function_name) {
 
-		code = do_python_single(NULL, inst->instantiate.function, "instantiate", inst->pass_all_vps, inst->pass_all_vps_dict, inst->utf8_fail_as_bytes);
+		code = do_python_single(NULL, inst->instantiate.function, "instantiate", inst->pass_all_vps, inst->pass_all_vps_dict);
 		if (code < 0) {
 		error:
 			python_error_log();	/* Needs valid thread with GIL */
@@ -1400,7 +1394,7 @@ static int mod_detach(void *instance)
 	 */
 	PyEval_RestoreThread(inst->sub_interpreter);
 
-	if (!check_config && inst->detach.function) ret = do_python_single(NULL, inst->detach.function, "detach", inst->pass_all_vps, inst->pass_all_vps_dict, inst->utf8_fail_as_bytes);
+	if (!check_config && inst->detach.function) ret = do_python_single(NULL, inst->detach.function, "detach", inst->pass_all_vps, inst->pass_all_vps_dict);
 
 #define PYTHON_FUNC_DESTROY(_x) python_function_destroy(&inst->_x)
 	PYTHON_FUNC_DESTROY(instantiate);
